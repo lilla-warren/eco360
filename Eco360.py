e3509c9f09080e8eb564ebdf5cbf1d539c0b134b
lilla-warren <lumiandflare@gmail.com>
2025-05-21 22:15:11 +0400

    Update Eco360.py

--- Eco360.py
+++ Eco360.py
@@ -34,7 +34,6 @@ df = pd.DataFrame(data)
 label_encoders = {}
 for column in df.columns:
     if df[column].dtype == 'object':
-        le = LabelEncoder()
         df[column] = le.fit_transform(df[column])
         label_encoders[column] = le
 
