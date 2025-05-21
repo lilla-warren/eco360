15726ae598f7b1636adf46a36c321f472d990eab
lilla-warren <lumiandflare@gmail.com>
2025-05-21 22:19:28 +0400

    Update Eco360.py

--- Eco360.py
+++ Eco360.py
@@ -3,6 +3,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
 # --- Page Config ---
 st.set_page_config(page_title="Eco360 – ML Spatial Predictor", layout="wide")
@@ -34,10 +35,6 @@ df = pd.DataFrame(data)
 label_encoders = {}
 for column in df.columns:
     if df[column].dtype == 'object':
-
-
-# Train/Test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # ML Model
 model = RandomForestClassifier(n_estimators=100, random_state=42)
